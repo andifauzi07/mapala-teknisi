292bfa0ec1fef94ab4e869644793681077c6cf29
andi fauzi <andifauzi0709@gmail.com>
2023-07-04 17:00:05 +0800

    fixed cta button in hero components

--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CTA = ({ text, title, name, children }) => {
 	return (
 		<div>
-			{/* You can open the modal using ID.showModal() method */}
+			{/* You can open the modal using ID.showModal() method
 			<button
 				className="btn bg-modal mt-5 text-white hover:text-slate-400"
 				onClick={() => window.my_modal_3.showModal()}>
@@ -19,7 +19,33 @@ const CTA = ({ text, title, name, children }) => {
 					<h3 className="font-bold text-lg">{title}</h3>
 					<p className="py-4">{children}</p>
 				</form>
-			</dialog>
+			</dialog> */}
+			{/* The button to open modal */}
+			<label
+				htmlFor="my_modal_6"
+				className="btn mt-5 text-white hover:text-slate-400">
+				{name}
+			</label>
+
+			{/* Put this part before </body> tag */}
+			<input
+				type="checkbox"
+				id="my_modal_6"
+				className="modal-toggle"
+			/>
+			<div className="modal">
+				<div className="modal-box">
+					<h3 className="font-bold text-lg">{title}</h3>
+					<p className="py-4">{children}</p>
+					<div className="modal-action">
+						<label
+							htmlFor="my_modal_6"
+							className="btn">
+							Close!
+						</label>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
