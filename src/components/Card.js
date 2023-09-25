37116ba9ad8acbdf1af6a47c6fb580370d53d26f
andi fauzi <andifauzi0709@gmail.com>
2023-09-25 19:41:13 +0800

    menambahkan gambar proker

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Card = ({ children, id }) => {
+const Card = ({ children, url }) => {
 	return (
 		<div>
-			<div className="card card-compact lg:w-96 bg-base-100 text-shadow text-white font-bold shadow-xl pt-10 border-2 border-black">
+			<div className="card card-compact lg:w-96 bg-base-100 text-shadow text-white font-bold shadow-xl border-2 border-black">
 				<figure>
 					<img
-						src={`https://picsum.photos/id/${id}/320/200`}
-						alt="News"
+						src={url}
+						alt={url}
 					/>
 				</figure>
 				<div className="card-body">{children}</div>
@@ -19,7 +19,7 @@ const Card = ({ children, id }) => {
 const Body = ({ title, children }) => {
 	return (
 		<>
-			<h2 className="card-title font-bold">{title}</h2>
+			<h2 className="card-title font-bold text-center">{title}</h2>
 			<p className=" italic"> {children} </p>
 			{/* <div className="card-actions justify-center">
 				<button className="btn bg-modal text-white hover:text-slate-400">Selengkapnya</button>
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,49 +1,45 @@
 import React from 'react';
 import Card from '../Card';
 import ContentLayout from '../Layout/ContentLayout';
+import proker1 from '../../assets/proker1.jpg';
+import proker2 from '../../assets/proker2.jpg';
+import proker3 from '../../assets/proker3.jpg';
+import proker4 from '../../assets/proker4.jpg';
+import proker5 from '../../assets/proker5.jpg';
+import proker6 from '../../assets/proker6.jpg';
 
 const News = () => {
 	return (
 		<ContentLayout>
 			<ContentLayout.Header>
-				<h1 className="text-3xl lg:text-5xl font-bold">Berita Terkini</h1>
+				<h1 className="text-3xl lg:text-5xl font-bold">Dewan Pengurus Harian</h1>
+				<h2 className="text-xl lg:text-3xl font-bold py-5">
+					Mapala <i>Teknisi</i> FT-UNM
+				</h2>
+				<h3 className="text-xl lg:text-3xl font-bold">Periode 2022 - 2023</h3>
 			</ContentLayout.Header>
 			<ContentLayout.Body gap="gap-6">
-				<Card id="34">
-					<Card.Body title="Penumpukan Sampah">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi reprehenderit ullam! Vitae, voluptatum eaque. At blanditiis beatae autem quibusdam dolore unde facilis. Atque, odit perspiciatis! Illo eum ipsa tempore!
-					</Card.Body>
+				<Card url={proker2}>
+					<Card.Body title="RAPAT KERJA DEWAN PENGURUS HARIAN MAPALA Teknisi Periode 2022-2023" />
 				</Card>
-				<Card id="62">
-					<Card.Body title="Pembakaran hutan">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi reprehenderit ullam! Vitae, voluptatum eaque. At blanditiis beatae autem quibusdam dolore unde facilis. Atque, odit perspiciatis! Illo eum ipsa tempore!
-					</Card.Body>
+				<Card url={proker3}>
+					<Card.Body title="ANGGOTA MUDA DIKSAR XXVII REINCARNATION MAPALA Teknisi FT-UNM" />
 				</Card>
-				<Card id="16">
-					<Card.Body title="Reklamasi pantai">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi reprehenderit ullam! Vitae, voluptatum eaque. At blanditiis beatae autem quibusdam dolore unde facilis. Atque, odit perspiciatis! Illo eum ipsa tempore!
-					</Card.Body>
+				<Card url={proker1}>
+					<Card.Body title="PELANTIKAN MAPALA Teknisi FT-UNM PERIODE 2022-2023" />
 				</Card>
-				<Card id="15">
-					<Card.Body title="Pencemaran Mata Air">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi reprehenderit ullam! Vitae, voluptatum eaque. At blanditiis beatae autem quibusdam dolore unde facilis. Atque, odit perspiciatis! Illo eum ipsa tempore!
-					</Card.Body>
+				{/* <Card url={proker4}>
+					<Card.Body title="Pencemaran Mata Air" />
 				</Card>
-				<Card id="46">
-					<Card.Body title="Daerah Gersang Sulawesi">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi reprehenderit ullam! Vitae, voluptatum eaque. At blanditiis beatae autem quibusdam dolore unde facilis. Atque, odit perspiciatis! Illo eum ipsa tempore!
-					</Card.Body>
+				<Card url={proker5}>
+					<Card.Body title="Daerah Gersang Sulawesi" />
 				</Card>
-				<Card id="47">
-					<Card.Body title="Limbah Merusak Pantai">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi reprehenderit ullam! Vitae, voluptatum eaque. At blanditiis beatae autem quibusdam dolore unde facilis. Atque, odit perspiciatis! Illo eum ipsa tempore!
-					</Card.Body>
+				<Card url={proker6}>
+					<Card.Body title="Limbah Merusak Pantai" />
 				</Card>
 				<Card id="166">
-					<Card.Body title="Keindahan Alam Yang Tersembunyi">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi reprehenderit ullam! Vitae, voluptatum eaque. At blanditiis beatae autem quibusdam dolore unde facilis. Atque, odit perspiciatis! Illo eum ipsa tempore!
-					</Card.Body>
-				</Card>
+					<Card.Body title="Keindahan Alam Yang Tersembunyi" />
+				</Card> */}
 			</ContentLayout.Body>
 		</ContentLayout>
 	);
